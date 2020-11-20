41db0c690c7e1dabe3f82cb41ac3b4194c8d41cb
Jason-S-Wu <30715413+Jason-S-Wu@users.noreply.github.com>
2020-11-20 17:39:09 -0500

    Update AnswerScript.js

--- AnswerScript.js
+++ AnswerScript.js
@@ -2,20 +2,20 @@
 
 if (problem.ansType == 2 || problem.ansType == 1) 
 {
-document.getElementsByClassName('submit-answer-form')[0].click();
-problem.ansType = 1;
-document.getElementsByClassName('btn btn-default cancel-button-float')[0].click();
-answer = problem.answers.valueOf();
-answerLength = answer.length;
-for (var i = 0; i < answerLength; i++) 
-{
-    document.getElementsByClassName('mq-editable-field')[0].innerText = answer[0];
-    document.querySelector("#innerAnswerForm > answer-form-one > div > div > b.glyphicon.glyphicon-plus-sign.no-select").click();
-    document.getElementsByClassName('mq-editable-field')[i].innerText = answer[i];
-}
+    document.getElementsByClassName('submit-answer-form')[0].click();
+    problem.ansType = 1;
+    document.getElementsByClassName('btn btn-default cancel-button-float')[0].click();
+    answer = problem.answers.valueOf();
+    answerLength = answer.length;
+    for (var i = 0; i < answerLength; i++) 
+    {
+        document.getElementsByClassName('mq-editable-field')[0].innerText = answer[0];
+        document.querySelector("#innerAnswerForm > answer-form-one > div > div > b.glyphicon.glyphicon-plus-sign.no-select").click();
+        document.getElementsByClassName('mq-editable-field')[i].innerText = answer[i];
+    }
     document.getElementsByClassName('submit-answer-form')[0].click();
 }
-if (problem.ansType == 'custom')
+    if (problem.ansType == 'custom')
 {
-alert('Module Not Supported')
+    alert('Module Not Supported')
 }
